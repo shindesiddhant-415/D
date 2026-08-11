02515aa60303856a50f686c9d0a2cd104c2292b4
Shinde siddhant <shindesiddhant415@gmail.com>
2026-08-11 23:44:33 +0530

    Time: 7 ms (55.10%) | Memory: 128.4 MB (33.89%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,33 +1,48 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
+        // int n=nums.size();
+        // int arr1[n/2];
+        // int arr2[n/2];
+        // int p=0,ne=0;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]>0){
+        //         arr1[p]=nums[i];
+        //         p++;
+        //     }else{
+        //         arr2[ne]=nums[i];
+        //         ne++;
+        //     }
+        // }
+
+        // p=0;
+        // ne=0;
+        
+        // for(int i=0;i<n;i++){
+        //     if(i%2==0){
+        //         nums[i]=arr1[p];
+        //         p++;
+        //     }else{
+        //         nums[i]=arr2[ne];
+        //         ne++;
+        //     }
+        // }
+        // return nums;
+
+
+
         int n=nums.size();
-        int arr1[n/2];
-        int arr2[n/2];
-        int p=0,ne=0;
+        vector<int>ans(n);
+        int e=0,ne=1;
         for(int i=0;i<n;i++){
             if(nums[i]>0){
-                arr1[p]=nums[i];
-                p++;
+                ans[e]=nums[i];
+                e+=2;
             }else{
-                arr2[ne]=nums[i];
-                ne++;
+                ans[ne]=nums[i];
+                ne+=2;
             }
         }
-
-        p=0;
-        ne=0;
-        
-        for(int i=0;i<n;i++){
-            if(i%2==0){
-                nums[i]=arr1[p];
-                p++;
-            }else{
-                nums[i]=arr2[ne];
-                ne++;
-            }
-        }
-        return nums;
-
+        return ans;
     }
 };
