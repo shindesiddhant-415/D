a4055059586313ef90ff1b7959c75b3adade6d59
Shinde siddhant <shindesiddhant415@gmail.com>
2026-09-02 00:50:28 +0530

    Time: 3 ms (38.07%) | Memory: 14.4 MB (32.47%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -4,30 +4,23 @@ public:
         unordered_map<int,int> mp1;
         unordered_map<int,int> mp2;
         vector<int> result;
-        set<int>s1;
-        set<int>s2;
-        for(int i=0;i<nums1.size();i++){
-            s1.insert(nums1[i]);
-        }
-        for(int i=0;i<nums2.size();i++){
-            s2.insert(nums2[i]);
-        }
-
-        for(auto it : s1){
+        for(auto it : nums1){
             mp1[it]=1;
         }
-        
-        for(auto it : s2){
-            mp2[it]=1;
-        }
-        for(auto it : s2){
-            if(mp1.find(it)!=mp1.end()){
+        for(auto it : nums2){
+            if(mp1.find(it)!=mp1.end() && mp2.find(it)==mp2.end()){
                 result.push_back(it);
-                
+                mp2[it]=1;
             }
         }
-         return result;
-        
+        // set<int>s1;
+        // set<int>s2;
+        // for(int i=0;i<nums1.size();i++){
+        //     s1.insert(nums1[i]);
+        // }
+        // for(int i=0;i<nums2.size();i++){
+        //     s2.insert(nums2[i]);
+        // }
 
         // vector<int>ans;
         // if(nums1.size())
@@ -37,6 +30,6 @@ public:
         //     }
         // }
         // return ans;
-       
+        return result;
     }
 };
