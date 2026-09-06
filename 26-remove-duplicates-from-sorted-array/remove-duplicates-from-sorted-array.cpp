752f68e1fe3c4a9d36eb810a81f23d4cf88c68e7
Shinde siddhant <shindesiddhant415@gmail.com>
2026-09-07 01:05:49 +0530

    Time: 0 ms (100.00%) | Memory: 22.4 MB (97.90%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -3,7 +3,7 @@ public:
     int removeDuplicates(vector<int>& nums) {
 
 
-        //teo-pointer 
+        //two-pointer 
         int i=0;
         for(int j=1;j<nums.size();j++){
             if(nums[j]!=nums[i]){
