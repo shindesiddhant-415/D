166bd0246de4def934f67dc11a66e2009d283cc0
Shinde siddhant <shindesiddhant415@gmail.com>
2026-09-09 01:10:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (79.89%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
+
+        //O(n^2)
+        // int count=0;
+        // for(int i=0;i<nums.size();i++){
+        //     for(int j=i;j<nums.size();j++){
+        //         if(nums[i]==nums[j] && i<j){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+
+        //O(n)-->hash map
         int count=0;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i;j<nums.size();j++){
-                if(nums[i]==nums[j] && i<j){
-                    count++;
-                }
-            }
+        unordered_map<int,int>mp;
+        for(auto n:nums){
+            count+=mp[n];
+            mp[n]++;
         }
         return count;
     }
