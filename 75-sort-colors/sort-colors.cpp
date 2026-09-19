290331bac8a9512ad17782835fefd467f975fa20
Shinde siddhant <shindesiddhant415@gmail.com>
2026-09-19 19:08:47 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (11.45%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int c0=0,c1=0,c2=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0) c0++;
-            else if(nums[i]==1) c1++;
-            else if(nums[i]==2) c2++;
+        int n=nums.size();
+        int low=0;
+        int mid=0;
+        int high=n-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
 
 
-        }
-        for(int i=0;i<c0;i++){
-            nums[i]=0;
-        }
-        for(int i=c0;i<c0+c1;i++){
-            nums[i]=1;
-        }
-        for(int i=c1+c0;i<c0+c1+c2;i++){
-            nums[i]=2;
-        }
 
-        
+        // int c0=0,c1=0,c2=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==0) c0++;
+        //     else if(nums[i]==1) c1++;
+        //     else if(nums[i]==2) c2++;
+        // }
+        // for(int i=0;i<c0;i++){
+        //     nums[i]=0;
+        // }
+        // for(int i=c0;i<c0+c1;i++){
+        //     nums[i]=1;
+        // }
+        // for(int i=c1+c0;i<c0+c1+c2;i++){
+        //     nums[i]=2;
+        // }      
     }
 };
