19f6d94b752e23b87c5c1186386d7c9d35ca1313
Shinde siddhant <shindesiddhant415@gmail.com>
2026-09-26 00:52:27 +0530

    Time: 4 ms (28.72%) | Memory: 31.8 MB (23.96%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.cpp
@@ -1,30 +1,50 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-        int n=nums.size();
-        int l=0;
-        int end=-1;
-        int start=0;
-        int maxv=nums[0];
-        int minv=nums[n-1];
 
-        for(int i=n-2;i>=0;i--){  //string potion of substring
-            if(nums[i]>minv){
-                start=i;
-            }else{
-                minv=nums[i];
+        int n = nums.size();
+        vector<int> store = nums;
+
+        sort(store.begin(),store.end());
+        int s = n;
+        int e = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i] != store[i]){
+                s = min(s,i);
+                e = max(e,i);
             }
         }
-
-        for(int i=0;i<n;i++){   //end of the substring 
-            if(nums[i]<maxv){
-                end=i;
-            }else{
-                maxv=nums[i];
-            }
+        if(e < s){
+            return 0;
+        }
+        else{
+            return e-s+1;
         }
 
-        if(end==-1) return 0;
-        else return end-start+1;
+        // int n=nums.size();
+        // int l=0;
+        // int end=-1;
+        // int start=0;
+        // int maxv=nums[0];
+        // int minv=nums[n-1];
+
+        // for(int i=n-2;i>=0;i--){  //string potion of substring
+        //     if(nums[i]>minv){
+        //         start=i;
+        //     }else{
+        //         minv=nums[i];
+        //     }
+        // }
+
+        // for(int i=0;i<n;i++){   //end of the substring 
+        //     if(nums[i]<maxv){
+        //         end=i;
+        //     }else{
+        //         maxv=nums[i];
+        //     }
+        // }
+
+        // if(end==-1) return 0;
+        // else return end-start+1;
     }
 };
